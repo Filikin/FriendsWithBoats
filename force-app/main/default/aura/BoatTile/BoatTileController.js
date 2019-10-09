d0e2e8037e8419f3240674fcb3caef9236761846
Filikin <eamon.kelly@enclude.ie>
2019-10-09 17:34:22 +0100

    Superbadge completedgit add -A!

--- force-app/main/default/aura/BoatTile/BoatTileController.js
+++ force-app/main/default/aura/BoatTile/BoatTileController.js
@@ -6,5 +6,11 @@
         var appEvent = $A.get("e.c:BoatSelected");
         appEvent.setParam ("boat", component.get("v.boat"));
         appEvent.fire();
+        var plotMapEvent = $A.get("e.c:PlotMapMarker");
+        plotMapEvent.setParam("sObjectId", component.get("v.boat.Id"));
+        plotMapEvent.setParam("lat", component.get("v.boat.Geolocation__Latitude__s"));
+        plotMapEvent.setParam("long", component.get("v.boat.Geolocation__Longitude__s"));
+        plotMapEvent.setParam("label", component.get("v.boat.Name"));
+        plotMapEvent.fire();
 	}
 })
--- force-app/main/default/aura/Map/MapController.js
+++ force-app/main/default/aura/Map/MapController.js
@@ -1,5 +1,14 @@
 ({
     jsLoaded: function(component) {
         component.set("v.jsLoaded", true);
-    }  
+    },
+    onBoatPlotted: function(component, event){
+        console.log ("on boat plotted");
+        console.log (event.getParam('label'));
+        var location= new Object();
+        location.lat = event.getParam('lat');
+        location.long = event.getParam('long');
+        console.log (location);
+        component.set("v.location", location);
+    }
 })
